c6b0ec811dd2b4e0a8be522660af82d3e89e9d71
julian <juliancanderson@gmail.com>
2023-09-01 12:52:28 +0700

    feat: adding typing

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ async fn health_check() -> HttpResponse { HttpResponse::Ok().finish() }
 // We return `Server` on the happy path and we dropped the `async` keyword // We have no .await call, so it is not needed anymore.
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-  let server = HttpServer::new(|| { 
+  let server: Server = HttpServer::new(|| { 
     App::new() 
       .route("/health_check", web::get().to(health_check))
   })
