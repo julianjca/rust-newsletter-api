c375d8ad7b35e17993bfc7da1a2eb18aa0febe20
julian <juliancanderson@gmail.com>
2023-09-01 09:19:40 +0700

    feat: updating function to run on random port

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 //! src/main.rs
 
 use zero2prod::run;
+use std::net::TcpListener;
 
 #[tokio::main] 
 async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    run()?.await 
+    let listener = TcpListener::bind("127.0.0.1:0") .expect("Failed to bind random port"); 
+    let port = listener.local_addr().unwrap().port();
+    println!("Server listening on http://localhost:{}", port);
+    run(listener)?.await?;
+    Ok(())
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,21 +1,23 @@
-fn spawn_app() {
-  let server = zero2prod::run().expect("Failed to bind address"); 
-  // Launch the server as a background task 
-  // tokio::spawn returns a handle to the spawned future, 
-  // but we have no use for it here, hence the non-binding let 
-  let _ = tokio::spawn(server);
+use std::net::TcpListener;
+fn spawn_app() -> String {
+  let listener = TcpListener::bind("127.0.0.1:0") .expect("Failed to bind random port"); 
+  // We retrieve the port assigned to us by the OS 
+  let port = listener.local_addr().unwrap().port(); let server = zero2prod::run(listener).expect("Failed to bind address"); 
+  let _ = tokio::spawn(server); 
+  // We return the application address to the caller! 
+  format!("http://127.0.0.1:{}", port)
 }
 
 #[tokio::test] 
 async fn health_check_works() {
   // spawning app in the background
-  spawn_app();  
+  let address = spawn_app();
 
   // create a new client to connect to the server
   let client = reqwest::Client::new();
 
   // send a GET request to the health_check endpoint
-  let response = client.get("http://127.0.0.1:8000/health_check") .send() .await .expect("Failed to execute request.");
+  let response = client.get(&format!("{}/health_check", &address)) .send() .await .expect("Failed to execute request.");
 
   // Assert
   assert!(response.status().is_success()); assert_eq!(Some(0), response.content_length());
