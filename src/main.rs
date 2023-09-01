8e3a86c70a8a41f40309f50441d4c8690144f05d
julian <juliancanderson@gmail.com>
2023-09-01 08:54:12 +0700

    feat: adding testing to endpoint + refactor codes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
+//! src/main.rs
 
-async fn health_check() -> impl Responder { HttpResponse::Ok().finish() }
+use zero2prod::run;
 
-#[tokio::main] async fn main() -> std::io::Result<()> {
-
-    HttpServer::new(|| { App::new()
-        .route("/health_check", web::get().to(health_check)) }) 
-        .bind("127.0.0.1:8000")?
-    
-    .run() .await
+#[tokio::main] 
+async fn main() -> std::io::Result<()> {
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    run()?.await 
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,22 @@
-//! tests/health_check.rs
+fn spawn_app() {
+  let server = zero2prod::run().expect("Failed to bind address"); 
+  // Launch the server as a background task 
+  // tokio::spawn returns a handle to the spawned future, 
+  // but we have no use for it here, hence the non-binding let 
+  let _ = tokio::spawn(server);
+}
 
-use zero2prod::main;
+#[tokio::test] 
+async fn health_check_works() {
+  // spawning app in the background
+  spawn_app();  
 
-#[test] fn dummy_test() { main() }
+  // create a new client to connect to the server
+  let client = reqwest::Client::new();
+
+  // send a GET request to the health_check endpoint
+  let response = client.get("http://127.0.0.1:8000/health_check") .send() .await .expect("Failed to execute request.");
+
+  // Assert
+  assert!(response.status().is_success()); assert_eq!(Some(0), response.content_length());
+}
