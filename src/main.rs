78d4c8da3ac5910f07c1f8735a1fb6d90a48e1a9
julian <juliancanderson@gmail.com>
2023-09-01 13:25:34 +0700

    chore: formatting

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 //! src/main.rs
 
-use zero2prod::run;
 use std::net::TcpListener;
+use zero2prod::run;
 
-#[tokio::main] 
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    let listener = TcpListener::bind("127.0.0.1:0") .expect("Failed to bind random port"); 
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     println!("Server listening on http://localhost:{}", port);
     run(listener)?.await?;
