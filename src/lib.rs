d4727ffb61264ea84967cf1a4be257f06d4d1acb
julian <juliancanderson@gmail.com>
2023-09-01 13:03:48 +0700

    feat: subscribe function and /subscriptions endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ use actix_web::{web, App, HttpResponse, HttpServer}; use actix_web::dev::Server;
 use std::net::TcpListener;
 
 async fn health_check() -> HttpResponse { HttpResponse::Ok().finish() }
+async fn subscribe() -> HttpResponse { HttpResponse::Ok().finish() }
 
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword // We have no .await call, so it is not needed anymore.
@@ -12,6 +13,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
   let server: Server = HttpServer::new(|| { 
     App::new() 
       .route("/health_check", web::get().to(health_check))
+      .route("/subscriptions", web::post().to(subscribe))
   })
   .listen(listener)?
   .run();
@@ -19,3 +21,4 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
   // No .await here! 
   Ok(server)
 }
+
