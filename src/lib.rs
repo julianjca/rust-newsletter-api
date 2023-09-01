e7c6df1987431400341e6495d1b6ffaa4d449772
julian <juliancanderson@gmail.com>
2023-09-01 14:01:02 +0700

    feat: form validation using serde

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,16 @@ use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
-async fn subscribe() -> HttpResponse {
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
